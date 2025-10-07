ee1c7ebc99a1a7cc60271ff49e99ae1a8cbd5759
Bril999 <brildima1@gmail.com>
2025-10-07 10:19:53 +0300

    Initial empty project

---
+++ ProjectFiles/main.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <math.h>
+
+#include "MDR32F9Qx_config.h"
+#include "MDR32F9Qx_usb_handlers.h"
+#include "MDR32F9Qx_rst_clk.h"
+#include "MDR32F9Qx_ssp.h"
+#include "MDR32F9Qx_port.h"
+#include "MDR32F9Qx_adc.h"
+#include "MDR32F9Qx_dac.h"
+#include "MDR32F9Qx_dma.h"
+#include "MDR32F9Qx_timer.h"
+
+
+int main(void) {
+    
+    while (1) {
+        
+    }
+}
